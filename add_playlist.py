5acd9d7324c2c95e30cad923719050876aef7075
Marnei Machado <marnei.machado@gmail.com>
2023-07-17 20:38:18 -0300

    feat: Add difflib
    Added difflib to display the accuracy score of the song matches

--- add_playlist.py
+++ add_playlist.py
@@ -2,6 +2,7 @@ import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 from dotenv import load_dotenv
 from os import getenv
+import difflib
 
 load_dotenv()
 
@@ -14,22 +15,37 @@ auth_manager = SpotifyOAuth(
 
 spotify = spotipy.Spotify(auth_manager=auth_manager)
 
+
+def similar(a, b):
+    similarity_score = difflib.SequenceMatcher(None, a, b).ratio()
+    return similarity_score, b
+
+
 with open('songs.txt', 'r') as f:
     songs = f.read().splitlines()
 
 song_ids = []
 
 for song in songs:
-    result = spotify.search(q=song, limit=1)
+    result = spotify.search(q=song, limit=5)
     if result['tracks']['total'] > 0:
+        best_match = None
+        best_match_score = 0
         for idx, track in enumerate(result['tracks']['items']):
-            print(idx, track['name'])
-            song_ids.append(track['id'])
+            similarity_score, track_name = similar(
+                song.lower(), track['name'].lower())
+            if similarity_score > best_match_score:
+                best_match = track
+                best_match_score = similarity_score
+
+        if best_match:
+            print(f"{best_match['name']} - {best_match_score * 100:.2f}%")
+            song_ids.append(best_match['id'])
+        else:
+            print(f"The music '{song}' was not found.")
     else:
-        print(f"A música '{song}' não foi encontrada.")
+        print(f"The music '{song}' was not found.")
 
 spotify.playlist_add_items(
-    playlist_id=getenv(
-        'YOUR_PLAYLIST_ID'),
-    items=song_ids
-)
+    playlist_id=getenv('YOUR_PLAYLIST_ID'),
+    items=song_ids)
